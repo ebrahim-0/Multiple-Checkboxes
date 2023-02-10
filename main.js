6cb0c1340cc17261601f3d37e39bd8aa7a71677a
Ebrahim Abd EL Razik <94077806+ebrahim-0@users.noreply.github.com>
2023-02-10 15:33:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const checkboxes = document.querySelectorAll('.item input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+  let isBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        isBetween = !isBetween;
+      }
+      if (isBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
